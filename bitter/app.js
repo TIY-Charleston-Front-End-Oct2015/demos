8aee3cc348fe192afc2215a6b6782c440779823f
Nathan Hall <nathan.mh@gmail.com>
2015-11-03 12:12:30 -0500

    added GET POST PUT DELETE to the repetoire

--- bitter/app.js
+++ bitter/app.js
@@ -13,18 +13,42 @@ var bitterPage = {
       bitterPage.createNewBitter();
     });
     $('.bitters').on('click','.delete',function (event) {
-      $(this).closest('article').remove();
+      var $deleteBtn = $(this)
+      var bitterID = $deleteBtn.closest('article').data('index');
+      bitterPage.deleteBitterInSpace(bitterID,$deleteBtn);
     })
     $('nav a').on('click', function (event) {
       event.preventDefault();
       var element = this
       bitterPage.showActiveSection(element);
     })
+    $('body').on('click','.edit', function(e) {
+      e.preventDefault();
+      var $editField = $(this).siblings('.edit-field')
+      $editField.css('display','inline-block');
+
+    })
+
+    $('body').on('click','input[name="edit"]', function(e) {
+      e.preventDefault();
+      var $submitBtn = $(this);
+      var bitterText = $submitBtn.siblings('input[name="bitter"]').val();
+      var avatar = $submitBtn.siblings('input[name="avatar"]').val();
+      var bitterID = $submitBtn.closest('article').data('index');
+      var editedBitter = {
+        avatar: avatar,
+        bitter: bitterText,
+        id: bitterID
+      }
+      bitterPage.editBitterInSpace(editedBitter,$submitBtn);
+    });
+
   },
   styling: function() {
-    bitterPage.loadBitters();
+    bitterPage.grabBittersFromSpace();
   },
-  loadBitters: function() {
+  url: "https://tiny-tiny.herokuapp.com/collections/bitters/",
+  loadBitters: function(bitterData) {
     var bitterHTML = "";
     _.each(bitterData, function (currVal, idx, arr) {
       bitterHTML += bitterPage.loadTemplate('bitterTmpl',currVal);
@@ -33,16 +57,17 @@ var bitterPage = {
     $('.bitters').html(bitterHTML);
   },
   createNewBitter: function() {
-    var avatarUrl = $('input[name="avatar"]').val();
-    var bitterText = $('input[name="bitter"]').val();
+    var avatarUrl = $('form > input[name="avatar"]').val();
+    var bitterText = $('form > input[name="bitter"]').val();
     var newBitter = {
       avatar: avatarUrl,
       bitter: bitterText
     };
-    bitterData.push(newBitter);
-    var htmlForArticle = bitterPage.loadTemplate('bitterTmpl',currItem);
-    $('.bitters').prepend(htmlForArticle);
-    $('input[type="text"]').val('');
+    console.log("NEW BITTER", newBitter)
+
+    bitterPage.sendBitterToSpace(newBitter);
+    // sendBitterToSpace
+
   },
   topThreeBitters: function() {
     var top3 = _.filter(bitterData, function (currItem, idx) {
@@ -62,6 +87,66 @@ var bitterPage = {
     var tmpl = bitterPage.getTemplate(name);
     return tmpl(val);
   },
+  sendBitterToSpace: function(bitter) {
+    console.log("IN TRANSIT", bitter);
+    $.ajax({
+      url: bitterPage.url,
+      method: 'POST',
+      data: bitter,
+      success: function(resp) {
+        var htmlForArticle = bitterPage.loadTemplate('bitterTmpl',resp);
+        $('.bitters').prepend(htmlForArticle);
+        $('form > input[type="text"]').val('');
+      },
+      failure: function(resp) {
+        console.log("FAILURE", resp);
+      }
+    });
+  },
+  grabBittersFromSpace: function() {
+    $.ajax({
+      type: 'GET',
+      url: bitterPage.url,
+      success: function(data) {
+        console.log("SUCCESS: ", data);
+        bitterPage.loadBitters(data);
+      },
+      failure: function(data) {
+        console.log("FAILURE: ", data);
+      }
+    });
+  },
+  editBitterInSpace: function(bitter,$editedField) {
+    $.ajax({
+      type: 'PUT',
+      url: bitterPage.url + bitter.id,
+      data: bitter,
+      success: function(editedBitter) {
+        console.log("I WAS CHANGED: ", editedBitter);
+        console.log($editedField);
+
+        $editedField.parent().css('display', 'none');
+        $editedField.parent().siblings('p').text(editedBitter.bitter);
+        $editedField.parent().siblings('h3').html('<img src="' + editedBitter.avatar +'">');
+      },
+      failure: function(editedBitter) {
+        console.log('IM STILL A FAILURE: ', editedBitter);
+      }
+    })
+  },
+  deleteBitterInSpace: function(bitterID,$editedField) {
+    $.ajax({
+      method: 'DELETE',
+      url: bitterPage.url + bitterID,
+      success: function(data) {
+        console.log("DELETED", data);
+        $editedField.closest('article').remove();
+      },
+      failure: function(data) {
+        console.log("ERROR", data);
+      }
+    });
+  }
 }
 
 
